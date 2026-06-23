0f84d1e45f2ac5b4323299998daa5e1c58132b47
isidro <isidro107109@gmail.com>
2026-06-23 19:42:17 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,162 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    
+    productos = db.relationship('Producto', backref='usuario', lazy=True)
+    
+    def serialize(self):
+        return{
+            "id":self.id,
+            "nombre":self.nombre
+        }
+
+class Producto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    
+    def to_dict(self):
+        return{
+            "id":self.id,
+            "nombre":self.nombre,
+            "precio":self.precio,
+            "usuario_id":self.usuario_id
+        }
+    
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return "Hola, esto funciona"
+
+@app.route('/usuario/register', methods=['POST'])
+def crear_usuario():
+    datos = request.json
+    
+    if not datos:
+        return jsonify({"error": "No se enviaron datos en formato JSON"})
+
+    if "nombre" in datos and "email" in datos:
+        nombre_limpio = datos["nombre"].strip()
+        email_limpio = datos["email"].strip()
+        if not nombre_limpio or len(nombre_limpio) > 50 or not email_limpio or len(email_limpio) > 100:
+            return jsonify({"error": "Los campos no esta correctamente completados"})
+    nuevo_usuario = Usuario(nombre=nombre_limpio, email=email_limpio)
+    db.session.add(nuevo_usuario)
+    db.session.commit()
+    
+    return jsonify({"mensaje": "Nuevo usuario creado",
+                    "Usuario":nuevo_usuario.serialize()})
+    
+@app.route('/registro', methods=['POST'])
+def registro():
+    
+    datos = request.json
+    
+    if not datos:
+        return jsonify({"error": "No se enviaron datos en formato JSON"}), 400
+    
+    if "nombre" not in datos:
+        return jsonify({"error": "Falta el campo 'nombre'"}), 400
+    
+    if "edad" not in datos:
+        return jsonify({"error": "Falta el campo 'edad'"}), 400
+    nombre = datos["nombre"]
+    edad = datos["edad"]
+    
+    return f"Usuario {nombre} de {edad} anhos registrado"
+
+@app.route('/productos', methods=['POST'])
+def crear_producto():
+    
+    datos = request.json
+    
+    if not datos:
+        return jsonify({"error": "No se enviaron datos en formato JSON"}), 400
+    
+    if "nombre" not in datos:
+        return jsonify({"error": "Falta el campo 'nombre'"}), 400
+    
+    if "precio" not in datos:
+        return jsonify({"error": "falta el campo 'precio'"}), 400
+    
+    nuevo = Producto(nombre=datos["nombre"], precio=datos["precio"])
+    
+    db.session.add(nuevo)
+    db.session.commit()
+    return jsonify({"mensaje": "Producto creado"}),201
+
+@app.route('/productos', methods=['GET'])
+def obtener_productos():
+    productos = Producto.query.all()
+    resultado = [p.to_dict() for p in productos]
+    return jsonify(resultado), 200
+
+@app.route('/productos/<int:id>', methods=['GET'])
+def obtener_producto(id):
+    producto = Producto.query.get(id)
+    
+    if not producto:
+        return jsonify({"error": "Este producto no existe"}), 404
+    
+    return jsonify(producto.to_dict()), 200
+
+@app.route('/productos/<int:id>', methods=['PUT'])
+def actualizar_producto(id):
+    
+    producto = Producto.query.get(id)
+    
+    if not producto:
+        return jsonify({"error": f"el id con el numero {id} no existe"}), 404
+    
+    data = request.json
+    
+    if not data:
+        return jsonify({"error": "No se enviaron datos en formato JSON"}), 400
+    
+    if "nombre" in data:
+        nombre_limpio = data["nombre"].strip()
+        if not nombre_limpio or len(nombre_limpio) > 40:
+            return jsonify({"mensaje":"El nombre no puede estar vacio o contener mas de 40 caracteres"}), 400
+        producto.nombre=nombre_limpio
+    
+    if "precio" in data:
+        if not isinstance(data["precio"],(int, float)):
+            return jsonify({"error": "El precio debe ser un numero"}), 400
+        producto.precio = data["precio"]
+        
+    db.session.commit()
+
+    return jsonify({"mensaje":"Tu Producto fue actualizado",
+                    "producto": producto.to_dict()}), 200
+    
+@app.route('/productos/<int:id>', methods=['DELETE'])
+def eliminar_producto(id):
+    
+    producto = Producto.query.get(id)
+    if producto is None:
+        return jsonify({"error": f"el id con el numero {id} no existe"}), 404
+    
+    nombre = producto.nombre
+    
+    db.session.delete(producto)
+    db.session.commit()
+    
+    return jsonify({"mensaje": f"El producto {nombre} fue eliminado"}), 200
+    
+@app.errorhandler(404)
+def no_encontrado(error):
+    return jsonify({"error": "Esa ruta no existe"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
