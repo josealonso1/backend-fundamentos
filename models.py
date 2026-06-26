222f2945d3b4612f4a6afc806f919ad354a54255
isidro <isidro107109@gmail.com>
2026-06-26 10:29:57 +0100

    Falta editar los routes registros

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     
     productos = db.relationship('Producto', backref='usuario', lazy=True, cascade='all, delete-orphan')
     
@@ -13,6 +15,12 @@ class Usuario(db.Model):
             "nombre":self.nombre,
             "email":self.email
         }
+        
+    def set_password(self, password_hash: str):
+        self.password = generate_password_hash(password_hash)
+        
+    def check_password(self, password_hash: str) -> bool:
+        return check_password_hash(self.password, password_hash)
 
 class Producto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
